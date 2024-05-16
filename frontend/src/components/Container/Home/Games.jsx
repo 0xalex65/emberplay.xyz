d2a4cce2319049c854728fb4fd147e8121c54c92
0xalex65 <0xalex65@gmail.com>
2024-05-17 03:54:42 +0800

    Style: updated game section content

--- frontend/src/components/Container/Home/Games.jsx
+++ frontend/src/components/Container/Home/Games.jsx
@@ -4,17 +4,12 @@ import { Link } from "react-router-dom";
 const Games = () => {
   return (
     <div className="flex flex-col items-center gap-6 md:gap-10 md:pb-20">
-      <h2 className="font-bold text-6xl md:text-7xl text-center">Games</h2>
+      <h2 className="font-bold text-6xl md:text-7xl text-center">
+        BET BIG, SOAR HIGHER
+      </h2>
       <p className="text-center text-lg opacity-80">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        ulti-game platform on Stargaze chain with an in-built revenue sharing
+        NFT collection, Ember. More to come...
       </p>
       <div className="w-full flex flex-col lg:flex-row justify-center gap-5 md:gap-20">
         <Link
